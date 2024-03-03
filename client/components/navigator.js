65d2668c2e2f7494d04b5899db99f39372353807
18div <divyamishra63618@gmail.com>
2024-03-03 22:17:28 +0530

    Login and signup with OTP added

--- client/components/navigator.js
+++ client/components/navigator.js
@@ -4,40 +4,62 @@ import { useSelector } from "react-redux";
 import BottomTabNavigator from "./bottomTabNavigator";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ErrorModal from "./errorModal";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/Login";
+import SignupScreen from "../screens/Signup";
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+       <Stack.Screen
+        name="Signup"
+        component={SignupScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const TabNavigator = ({ networkError, setNetworkError }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true); 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const userState = useSelector((state) => state.user);
+  const userState = useSelector((state) => state.user);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const existingUser = await AsyncStorage.getItem("user");
-                if (existingUser) {
-                    setUser(JSON.parse(existingUser));
-                }
-            } catch (error) {
-                console.error("Error fetching user:", error);
-            } finally {
-                setLoading(false); 
-            }
-        };
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const existingUser = await AsyncStorage.getItem("user");
+        if (existingUser) {
+          setUser(JSON.parse(existingUser));
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchUser();
-    }, [userState]);
+    fetchUser();
+  }, [userState]);
 
-    if (loading) {
-        return <Text>Fetching data</Text>;
-    }
+  if (loading) {
+    return <Text>Fetching data</Text>;
+  }
 
-    return (
-        <View style={{ flex: 1 }}>
-            <ErrorModal visible={networkError} onClose={() => setNetworkError(false)} />
-            {user ? <BottomTabNavigator /> : <AuthStack />}
-        </View>
-    );
+  return (
+    <View style={{ flex: 1 }}>
+      <ErrorModal visible={networkError} onClose={() => setNetworkError(false)} />
+      {user ? <BottomTabNavigator /> : <AuthStack />}
+    </View>
+  );
 };
 
 export default TabNavigator;
--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGO_URI;
+console.log(MONGODB_URI)
 mongoose.connect(MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -25,7 +26,7 @@ app.use('/user', userRoutes);
 // app.use('/group', groupRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3070;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
